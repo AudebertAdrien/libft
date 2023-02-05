244be839903f21d9f2ea250ffbca945e83f468c0
Adrien Audebert <aaudeber@c2r4p3.42perpignan.fr>
2023-02-05 17:17:59 +0100

    ft_atoi done

--- libft/src/ft_atoi.c
+++ libft/src/ft_atoi.c
@@ -6,12 +6,30 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 16:08:21 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/05 16:08:48 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/02/05 17:16:26 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *nptr)
 {
+	int	num;
+	int	sign;
+	int	i;
 
-	return (0);
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (nptr[i] && nptr[i] >= 10 && nptr[i] <= 13 || nptr[i] == 32)
+			i++;
+	if (nptr[i] && nptr[i] == 45) 
+	{
+		sign = sign * -1;
+		i++;
+	}
+	while (nptr[i] && nptr[i] >= 48 && nptr[i] <= 57)
+	{
+		num = (num * 10) + (nptr[i] - 48);	
+		i++;
+	}
+	return (num * sign);
 }
