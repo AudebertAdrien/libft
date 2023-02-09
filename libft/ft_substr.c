fc7169dfdceef87fc3518fc36c4c99447d8bac2a
Adrien Audebert <aaudeber@c2r5p2.42perpignan.fr>
2023-02-09 11:19:25 +0100

    clean substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:38:29 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/08 18:25:51 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/02/09 11:19:04 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	i = 0;
 	src_size = ft_strlen(s);
-
 	if (start >= src_size)
 		return (malloc(1));
-
 	if (len > src_size - start)
 		len = src_size - start;
-
 	ptr = (char *)malloc(sizeof(char) * (len + 1));
 	if (!ptr)
 		return (NULL);
-
 	while (i < len && s[start + i])
 	{
 		ptr[i] = s[start + i];
 		i++;
 	}
-	ptr[i] = '\0';	
+	ptr[i] = '\0';
 	return (ptr);
 }
