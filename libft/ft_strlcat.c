7e4cb254cec5f30da68a119b6e909cf0835f9526
Adrien Audebert <aaudeber@c3r3p2.42perpignan.fr>
2023-02-12 20:10:06 +0100

    add comment to function

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,11 +6,29 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 14:20:14 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/07 18:10:40 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/02/12 20:04:07 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+	DESCRIPTION
+	The strlcat() functions concatenate strings. 
+	Strlcat() take the full size of the buffer (not just the length) 
+	and guarantee to NUL-terminate the result (as long as size is larger 
+	than 0 or, in the case of strlcat(), as long as there is at least 
+	one byte free in dst)
+	Note that a byte for the NUL should be included in size
+	The strlcat() function appends the NUL-terminated string src to 
+	the end of dst.  It will append at most size - strlen(dst) - 1 bytes, 
+	NUL-terminating the result.
+
+	RETURN VALUES
+	The strlcpy() and strlcat() functions return the total length of the 
+	string they tried to create.
+	For strlcat() that means the initial length of dst plus the length of src.
+*/
 
 size_t	ft_strlcat(char *dst, char *src, size_t size)
 {
--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,11 +6,25 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 14:54:31 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/02/07 10:00:15 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/02/12 19:45:55 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+	DESCRIPTION
+	The strnstr() function locates the first occurrence of the null-terminated 
+	string little in the string big, where not more than len characters 
+	are searched.  Characters that appear after a ‘\0’ character are not
+     	searched.  Since the strnstr() function is a FreeBSD specific API, 
+	it should only be used when portability is not a concern.
+
+	RETURN VALUES
+     	If little is an empty string, big is returned; if little occurs 
+	nowhere in big, NULL is returned; otherwise a
+     	pointer to the first character of the first occurrence of little is returned.
+*/
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
